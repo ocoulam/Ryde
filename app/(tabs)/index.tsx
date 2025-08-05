2becf013ee88240d72f8ba066175f26868cd745b
ocoulam <owen.coulam.1@gmail.com>
2025-08-05 18:31:28 -0500

    Fixed dependency issues

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
-import { Text, View } from 'react-native';
-import "./global.css";
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
  
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+        Uber clone
       </Text>
-    </View>
+    </SafeAreaView>
   );
 }
